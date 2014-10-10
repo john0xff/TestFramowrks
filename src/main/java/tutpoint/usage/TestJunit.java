67e203f877025ec206e82215cf4b010f88a0076d
BartBien <phoenixjcam@gmail.com>
2014-10-10 21:33:28 +0200

    JUnit Basic Usage. Print msg, Test this msg and TestRunner

---
+++ src/main/java/tutpoint/usage/TestJunit.java
@@ -0,0 +1,27 @@
+package tutpoint.usage;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit
+{
+
+	String message = "Hello World";
+	MessageUtil messageUtil = new MessageUtil(message);
+
+	@Test
+	public void testPrintMessage()
+	{
+		/**
+		 * <pre>
+		 *  Hello World 
+		 *  testPrintMessage(tutpoint.usage.TestJunit): expected:<[New Wor]d> but was:<[Hello Worl]d> 
+		 *  false
+		 * </pre>
+		 *
+		 */
+		// message = "New World";
+
+		assertEquals(message, messageUtil.printMessage());
+	}
+}
