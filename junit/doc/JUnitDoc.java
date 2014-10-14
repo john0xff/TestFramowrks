4b9a091e897369aab667b6fc2521041922eda389
BartBien <phoenixjcam@gmail.com>
2014-10-14 09:19:23 +0200

    Doc to junit

---
+++ junit/doc/JUnitDoc.java
@@ -0,0 +1,16 @@
+package doc;
+
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.runners.JUnit4;
+
+public class JUnitDoc
+{
+	//JUnit4
+	
+	//JUnitMatchers
+	
+	//@Test
+	
+	
+}
