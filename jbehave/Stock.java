6292834c52336ec96852d3d9a5aaa585bdb7845c
BartBien <phoenixjcam@gmail.com>
2014-10-14 09:19:14 +0200

    JBehave samples.

---
+++ jbehave/Stock.java
@@ -0,0 +1,27 @@
+public class Stock
+{
+
+	public Stock(String symbol, double threshold)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getStatus()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Object name()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void tradeAt(double price)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
