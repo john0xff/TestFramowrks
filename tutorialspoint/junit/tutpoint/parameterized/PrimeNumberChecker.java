126973e29496befe5e49ec0b908bec07714c1fec
BartBien <phoenixjcam@gmail.com>
2014-10-15 09:58:20 +0200

    JUnit - Parameterized test

---
+++ tutorialspoint/junit/tutpoint/parameterized/PrimeNumberChecker.java
@@ -0,0 +1,16 @@
+package junit.tutpoint.parameterized;
+
+public class PrimeNumberChecker
+{
+	public Boolean validate(final Integer primeNumber)
+	{
+		for (int i = 2; i < (primeNumber / 2); i++)
+		{
+			if (primeNumber % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+}
